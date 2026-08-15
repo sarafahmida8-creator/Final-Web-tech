2007831a66575032b475e062eaf2238b205fe9b6
Sara <sarafahmida8@gmail.com>
2026-08-15 21:27:44 +0600

    Add files via upload

---
+++ Final Lab task3/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Registration</title>
+</head>
+
+<body>
+
+<h2>Student Registration System</h2>
+
+<form action="submit.php" method="POST">
+
+    Student Name:
+    <input type="text" name="student_name">
+    <br><br>
+
+    Student ID:
+    <input type="text" name="student_id">
+    <br><br>
+
+    Email:
+    <input type="email" name="email">
+    <br><br>
+
+    Department:
+    <input type="text" name="department">
+    <br><br>
+
+    <input type="submit" value="Register">
+
+</form>
+
+</body>
+</html>
